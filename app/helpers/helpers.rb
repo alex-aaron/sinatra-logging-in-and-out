361c48069f4fe8b035fe78fb7105c0c4a350a605
Alex <alexfrancisaaron@gmail.com>
2021-01-31 13:25:26 -0600

    commit tree 636e77affb4c73a994de34582c6b0a8569f5d379

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
+require 'pry'
 class Helpers
+
+    def self.current_user(session)
+        user_id = session[:user_id]
+        user = User.find(user_id)
+        user
+    end
+
+    def self.is_logged_in?(session)
+        if session[:user_id]
+            true
+        else
+            false
+        end
+    end
 end
